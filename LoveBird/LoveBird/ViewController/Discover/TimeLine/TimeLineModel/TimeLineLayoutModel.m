9feecbc86355ff086479954dda719c02c42c817a
cheli shan <924906515@qq.com>
2018-08-18 18:01:31 +0800

    adfadf

--- LoveBird/LoveBird/ViewController/Discover/TimeLine/TimeLineModel/TimeLineLayoutModel.m
+++ LoveBird/LoveBird/ViewController/Discover/TimeLine/TimeLineModel/TimeLineLayoutModel.m
@@ -32,18 +32,18 @@
 
     // 文章标题
     CGFloat titleLabelHeight = [contentModel.subject getTextHeightWithFont:kFontDiscoverTitle withWidth:(SCREEN_WIDTH - AutoSize(26))];
-    _titleLabelFrame = CGRectMake(AutoSize(13), height + AutoSize(10), SCREEN_WIDTH - AutoSize(26), titleLabelHeight);
+    _titleLabelFrame = CGRectMake(AutoSize(13), height + AutoSize(15), SCREEN_WIDTH - AutoSize(26), titleLabelHeight);
     height = CGRectGetMaxY(_titleLabelFrame);
 
     
     //文章内容 不一定存在
     if (contentModel.summary.length) {
         CGFloat contentLabelHeight = [contentModel.summary getTextHeightWithFont:kFontDiscoverContent withWidth:_titleLabelFrame.size.width];
-        _contentLabelFrame = CGRectMake(_titleLabelFrame.origin.x, height + AutoSize(10), _titleLabelFrame.size.width, contentLabelHeight);
+        _contentLabelFrame = CGRectMake(_titleLabelFrame.origin.x, height + AutoSize(15), _titleLabelFrame.size.width, contentLabelHeight);
         height = CGRectGetMaxY(_contentLabelFrame);
     }
 
-    height = height + AutoSize(10);
+    height = height + AutoSize(15);
     
     // 底部空白
     _bottomViewFrame = CGRectMake(_titleLabelFrame.origin.x, height, _titleLabelFrame.size.width, 0.5);
@@ -79,18 +79,18 @@
     
     // 文章标题
     CGFloat titleLabelHeight = [zhuangbeiModel.title getTextHeightWithFont:kFontDiscoverTitle withWidth:(SCREEN_WIDTH - AutoSize(26))];
-    _titleLabelFrame = CGRectMake(AutoSize(13), height + AutoSize(10), SCREEN_WIDTH - AutoSize(26), titleLabelHeight);
+    _titleLabelFrame = CGRectMake(AutoSize(13), height + AutoSize(15), SCREEN_WIDTH - AutoSize(26), titleLabelHeight);
     height = CGRectGetMaxY(_titleLabelFrame);
     
     
     //文章内容 不一定存在
     if (zhuangbeiModel.summary.length) {
         CGFloat contentLabelHeight = [zhuangbeiModel.summary getTextHeightWithFont:kFontDiscoverContent withWidth:_titleLabelFrame.size.width];
-        _contentLabelFrame = CGRectMake(_titleLabelFrame.origin.x, height + AutoSize(10), _titleLabelFrame.size.width, contentLabelHeight);
+        _contentLabelFrame = CGRectMake(_titleLabelFrame.origin.x, height + AutoSize(15), _titleLabelFrame.size.width, contentLabelHeight);
         height = CGRectGetMaxY(_contentLabelFrame);
     }
     
-    height = height + AutoSize(10);
+    height = height + AutoSize(15);
     
     // 底部空白
     _bottomViewFrame = CGRectMake(_titleLabelFrame.origin.x, height, _titleLabelFrame.size.width, 1);
